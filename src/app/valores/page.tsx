170616722d4fb1983575835c0b1978dd75135cb7
marciotrindadefilho <marciotrindadefilho629@gmail.com>
2025-08-02 01:58:11 -0300

    src/app/depoimentos/, src/app/missao/

--- src/app/valores/page.tsx
+++ src/app/valores/page.tsx
@@ -1,4 +1,8 @@
+"use client"
 
+import Image from "next/image"
+import Link from "next/link"
+import { Button } from "@/components/ui/button"
 
 export default function Values() {
   const values = [
